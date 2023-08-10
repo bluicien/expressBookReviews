b869ca5aad2b7862ab7259e131719d4f8b9b9642
Brendon <bluicien@yahoo.co.nz>
2023-08-10 07:14:47 -0400

    Regular update

--- final_project/index.js
+++ final_project/index.js
@@ -12,9 +12,31 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken']
+        jwt.verify(token, 'access', (err, user) => {
+            if (!err) {
+                req.customer = customer
+                next();
+            } else {
+                return res.status(403).json({message: "Customer not authenticated"})
+            }
+        })
+    } else {
+        return res.status(403).json({message: "Customer not logged in"})
+    }
 });
+
+app.post("/login", (req, res) => {
+    const customer = req.body.customer
+    if (!customer) {
+        return res.status(403).json({message: "No user body"})
+    }
+    let accessToken = jwt.sign({data: customer}, 'access', {expiresIn: 60 * 60})
+    req.session.authorization = {accessToken}
+})
  
-const PORT =5000;
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,21 +12,40 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    if (books) {
+        res.status(200).send(JSON.stringify({books}))
+    } else {
+        res.status(404).json({message: "Database not found!"});}
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let num = req.params.isbn
+    if (books[num]) {
+        res.status(200).send(JSON.stringify(books[num]))
+    } else {
+        res.status(402).json({message: "ISBN does not exist!"})
+    }
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const authorParam = req.params.author
+    let authorMatch
+    let isbn = Object.keys(books)    
+    isbn.forEach(num => {
+        if (books[num].author == authorParam) { 
+            return authorMatch = books[num]
+            }
+        })
+    if (authorMatch) {
+        return res.status(200).send(authorMatch)
+    } else {
+        return res.status(402).json({message: "No such author found"});
+    }
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
