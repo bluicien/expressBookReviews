143fb989b6c7a155c255dcfff4c57236f91018b6
Brendon <bluicien@yahoo.co.nz>
2023-08-11 07:59:34 -0400

    Update

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -17,37 +17,54 @@ const isValid = (username)=>{
 }
 
 const authenticatedUser = (username,password)=>{
-    users.forEach(user => {
-        if (user.username == username && user.password == password) {
-            return true
-        }
+    let validated = users.filter(user => {
+        return (user.username === username && user.password === password)
     })
+    if (validated.length > 0) {
+        return true
+    } else {
+        return false
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
     const username = req.query.username;
     const password = req.query.password;
-
-    if (!username || password) {
-        return res.status(402).json({message: "Please enter a username and password"})
+    if (!username || !password) {
+        return res.status(401).json({message: "Please enter a username and password"})
     } 
     if (authenticatedUser(username, password)) {
-
     let accessToken = jwt.sign({
         data: password
     }, 'access', {expiresIn: 60 * 60})
-    req.session.authorization = {accessToken, username}
+    req.session.authorization = {
+        accessToken, username
+    }
     return res.status(200).send("User successfully logged in");
 } else {
-    return res.status(402).json({message: "Invalid username or password"});
+    return res.status(401).json({message: "Invalid username or password"});
 }
     });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn
+    const username = req.session.authorization['username']
+    let customerReview = {
+        "username": username,
+        "review": req.query.givereview
+    }
+    if (customerReview) {
+        if (!books[isbn].review.username) {
+            books[isbn].review.push(customerReview)
+            return res.status(200).json({message: "Review posted successfully!"})
+        } else {
+            books[isbn].review.
+        }
+    } else {
+        return res.status(400).json({message: "Please enter a review message"});
+    }
 });
 
 module.exports.authenticated = regd_users;
