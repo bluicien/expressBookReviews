ee2312982ba91aefda74f983de488a3c3a62579c
Brendon <bluicien@yahoo.co.nz>
2023-08-12 03:57:43 -0400

    Completed assessment

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,55 +21,99 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    if (books) {
-        res.status(200).send(JSON.stringify({books}))
-    } else {
-        res.status(404).json({message: "Database not found!"});}
+    booksPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(res.status(200).send(JSON.stringify({books})))
+        }, );
+    })
+    booksPromise.then(() => {
+        console.log("Books retrieved successfully")
+    })
 });
 
-// Get book details based on ISBN
+        //=============== Alt code ===========//
+        // if (books) {
+        //     resolve(res.status(200).send(JSON.stringify({books})))
+        // } else {
+        //     reject(res.status(404).json({message: "Database not found!"}))
+        // }
+
+// =====================NON ASYNC CODE ==================
+    // if (books) {
+    //     res.status(200).send(JSON.stringify({books}))
+    // } else {
+    //     res.status(404).json({message: "Database not found!"});}
+
+
+// Get book details based on ISBN 
+//Using ASYNC function
 public_users.get('/isbn/:isbn',function (req, res) {
-    let num = req.params.isbn
-    if (books[num]) {
-        res.status(200).send(JSON.stringify(books[num]))
-    } else {
-        res.status(402).json({message: "ISBN does not exist!"})
+    const findByIsbn = async () => {
+        try {
+            if (books[num]) {
+                let num = (req.params.isbn)
+                const isbnDetails = await books[num]
+                res.status(200).send(JSON.stringify(isbnDetails))
+            } else {
+                res.status(402).json({message: "ISBN does not exist!"})
+            }
+            } catch (error) {
+            console.log(error)
+        }
     }
- });
-  
+    findByIsbn()
+});
+
+// ===============NON ASYNC CODE ================
+// if (books[num]) {
+//     res.status(200).send(JSON.stringify(books[num]))
+// } else {
+//     res.status(402).json({message: "ISBN does not exist!"})
+// }
+
 let keyId = Object.keys(books)    
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const authorParam = req.params.author
-    let authorMatch
-    keyId.forEach(num => {
-        if (books[num].author == authorParam) { 
-            return authorMatch = books[num]
-            }
-        })
-    if (authorMatch) {
-        return res.status(200).send(JSON.stringify(authorMatch))
-    } else {
-        return res.status(402).json({message: "No such author found"});
-    }
+    const authorPromise = new Promise((resolve, reject) => {
+        const authorParam = req.params.author
+        let authorMatch
+        keyId.forEach(num => {
+            if (books[num].author == authorParam) { 
+                return authorMatch = books[num]
+                }
+            })
+        if (authorMatch) {
+            resolve( res.status(200).send(JSON.stringify(authorMatch)))
+        } else {
+            return res.status(402).json({message: "No such author found"});
+        }
+    })
+    authorPromise.then(() => {
+        console.log("Match found")
+    })
 });
 
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const titleParam = req.params.title
-    let titleMatch
-    keyId.forEach(num => {
-        if (books[num].title == titleParam) { 
-            return titleMatch = books[num]
-            }
-        })
-    if (titleMatch) {
-        return res.status(200).send(JSON.stringify(titleMatch))
-    } else {
-        return res.status(402).json({message: "No such title found"});
-    }
+    const titlePromise = new Promise((resolve, rejeect) => {
+        const titleParam = req.params.title
+        let titleMatch
+        keyId.forEach(num => {
+            if (books[num].title == titleParam) { 
+                return titleMatch = books[num]
+                }
+            })
+        if (titleMatch) {
+            resolve (res.status(200).send(JSON.stringify(titleMatch)))
+        } else {
+            return res.status(402).json({message: "No such title found"});
+        }
+    })
+    titlePromise.then(() => {
+        console.log("Match found")
+    })
 });
 
 //  Get book review
